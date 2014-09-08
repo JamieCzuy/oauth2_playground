9564b731875e4b043665714fd97eee0ad09359dc
Jamie Czuy <jamie.czuy@gmail.com>
2014-09-07 23:54:33 -0400

    Protect the protected page

--- oauth2_resource_server/server.js
+++ oauth2_resource_server/server.js
@@ -1,5 +1,8 @@
 console.log('OAuth2_Resource_Server');
 var express = require('express');
+var passport = require('passport');
+var OAuth2Strategy = require('passport-oauth2').Strategy;
+
 
 var app = module.export = express();
 
@@ -13,10 +16,28 @@ app.get('/public/page1', function(req, res, next) {
   next();
 });
 
-app.get('/protected/page1', function(req, res, next) {
+app.get('/protected/page1', passport.authenticate('oauth2'),  function(req, res, next) {
   res.send('Protected Page #1 is not yet Protected');
   next();
 });
+
+
+newStrategy = function(strategy) {
+  return new OAuth2Strategy({
+    authorizationURL: 'https://localhost:3002/oauth2/authorize',
+    tokenURL: 'http://localhost:3002/oauth2/token',
+    clientID: 'oauth2_client_id',
+    clientSecret: 'oauth2_client_secret',
+    callbackURL: 'https://localhost:3001/oauth2/callback'
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // ---- User.findOrCreate ---- //
+    console.log('User has been Authenticated');
+    return done({username: 'OAuth2 User'});
+  });
+}
+passport.use(newStrategy());
+
 
 var PORT = 3001;
 console.log('Listening on port: ' + PORT);
